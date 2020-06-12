7dc536d13c9dc7fb5a87841e02e280a820c43e2e
ktalha <66550263+ktalha@users.noreply.github.com>
2020-06-12 11:15:32 +0100

    Update script.py

--- script.py
+++ script.py
@@ -19,7 +19,7 @@ def get_traveler_location(traveler):
 # test_destination_index = get_traveler_location(test_traveler)
 # print(test_destination_index)
 
-# first methos : List of emty lists:
+# first methos: declare a List of emty lists:
 # attractions = [[], [], [], [], []]
 
 # second method: Foor loop
@@ -27,6 +27,39 @@ def get_traveler_location(traveler):
 # for destination in destinations:
 #   attractions.append([])
 
-# third method using List comprehentions:
+# third method: using List comprehentions:
 attractions = [ [] for destination in destinations ]
 #print(attractions)
+
+def add_attraction(destination, attraction):
+  try:
+    destination_index = get_destination_index(destination)
+    attractions_for_destination = attractions[destination_index].append(attraction)
+  except SyntaxError: 
+    return
+
+add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
+#print(attractions)
+
+add_attraction("Los Angeles, USA", ['Griffith Observatory', ['Park']])
+# print(attractions)
+  
+add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+#print(attractions)
+
+
+
+
+
+
+
