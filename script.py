40dcc1a0cafabe9484f5352303df21aaf0459cc5
ktalha <66550263+ktalha@users.noreply.github.com>
2020-06-12 10:17:22 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,12 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
 
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+def get_destination_index(destination):
+  destination_index = destinations.index(destination)
+  return destination_index
+
+# index = get_destination_index("Los Angeles, USA")
+# print(index)
+# print(get_destination_index("Paris, France"))
+# print(get_destination_index("Hyderabad, India"))
