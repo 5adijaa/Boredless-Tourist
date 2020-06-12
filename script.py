c8dd927f782cd8691d93c0b58cc072363ee709eb
ktalha <66550263+ktalha@users.noreply.github.com>
2020-06-12 13:44:03 +0100

    Update script.py

--- script.py
+++ script.py
@@ -74,7 +74,25 @@ def find_attractions(destination, interests):
 la_arts = find_attractions('Los Angeles, USA', ['art'])
 print(la_arts)
 
+def get_attractions_for_traveler(traveler):
+  traveler_destination = traveler[1]
+  traveler_interests = traveler[2]
+  traveler_attractions = find_attractions(traveler_destination, traveler_interests)
+  interests_string = "Hi " + traveler[0] + ", we think you'll like these places around " + traveler_destination + ":"
+  
+  # for attraction_place in traveler_attractions:
+  #   interests_string += " the " + attraction_place + ","
+  #   return interests_string
+  
+  for attraction_place in range(len(traveler_attractions)):
+    if traveler_attractions[-1] == traveler_attractions[attraction_place]:
+      interests_string += " the " + traveler_attractions[attraction_place] + "."
+    else:
+      interests_string += " the " + traveler_attractions[attraction_place] + ","
+    return interests_string
 
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
+print(smills_france)
 
 
 
